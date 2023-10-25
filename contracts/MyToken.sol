19535ea603f365a2b5ec5be07924f467e8a8d87e
tom <tomasguerraalda@hotmail.com>
2023-10-25 14:16:01 -0300

    add ERC20 script

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor()
+        ERC20("MyToken", "MTK")
+    {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
---
+++ scripts/deployMyToken.js
@@ -0,0 +1,23 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script> --network <network-name>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const vestingContract = await hre.ethers.getContractFactory("MyToken");
+  const contract = await vestingContract.deploy();
+
+  const contractAddr = await contract.getAddress()
+
+  console.log(`MyToken contract deployed at ${contractAddr}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
