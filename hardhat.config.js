2968c1a588afaafe868f39f83b6a193fed004ec1
tom <tomasguerraalda@hotmail.com>
2023-10-25 14:15:09 -0300

    add testnet and mainnet configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,17 @@ module.exports = {
     evmoslocal: {
       url: "http://127.0.0.1:8545",
       chainId: 9000,
-      accounts: [process.env.ACCOUNT_KEY],
+      accounts: process.env.ACCOUNT_KEYS.split(","),
+    },
+    evmostestnet: {
+      url: "https://evmos-testnet.lava.build",
+      chainId: 9000,
+      accounts: process.env.ACCOUNT_KEYS.split(","),
+    },
+    evmosmainnet: {
+      url: "https://evmos.lava.build",
+      chainId: 9001,
+      accounts: process.env.ACCOUNT_KEYS.split(","),
     },
   },
 };
